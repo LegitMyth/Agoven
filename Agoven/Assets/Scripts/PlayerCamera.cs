8fa88c758c7d7397015b38010c4cfa32fbaa7479
Uphillcarp <uphillcarp@gmail.com>
2021-07-12 20:09:54 -0400

    Camera actually working

--- Agoven/Assets/Scripts/PlayerCamera.cs
+++ Agoven/Assets/Scripts/PlayerCamera.cs
@@ -6,6 +6,9 @@ public class PlayerCamera : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
+    public Vector3 previousPosition;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform target;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,23 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.position + offset;
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = previousPosition - cam.ScreenToViewportPoint(Input.mousePosition);
+
+            cam.transform.position = target.position;
+
+            cam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+            cam.transform.Rotate(new Vector3(0, 1, 0), direction.y * 180);
+            cam.transform.Translate(new Vector3(0, 0, -10));
+
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
     }
 }
